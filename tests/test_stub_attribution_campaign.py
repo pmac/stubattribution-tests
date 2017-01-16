a69d8228660731b966ed3bb6c6bd677be73c9465
Stephen Donner <stephen.donner@gmail.com>
2017-01-15 19:29:21 -0800

    More PEP8 fixes

--- tests/test_stub_attribution_campaign.py
+++ tests/test_stub_attribution_campaign.py
@@ -1,9 +1,7 @@
 import os
 import urlparse
-import urllib
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
--- tests/test_stub_attribution_search.py
+++ tests/test_stub_attribution_search.py
@@ -1,8 +1,6 @@
 import os
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
