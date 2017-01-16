936907440620246b259a770b99225f72132824b4
Stephen Donner <stephen.donner@gmail.com>
2017-01-15 20:06:55 -0800

    Add pytest

--- tests/test_stub_attribution_campaign.py
+++ tests/test_stub_attribution_campaign.py
@@ -1,8 +1,8 @@
 import os
+import pytest
 import urlparse
 
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
