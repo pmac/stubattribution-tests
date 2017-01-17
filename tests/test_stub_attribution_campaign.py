3b80e69df1d426ab03da17a157eb4d0c10569930
Stephen Donner <stephen.donner@gmail.com>
2017-01-17 13:13:27 -0800

    Fixed WebDriverWait for downloadButton

--- tests/test_stub_attribution_campaign.py
+++ tests/test_stub_attribution_campaign.py
@@ -32,11 +32,13 @@ def generate_url(source, medium, campaign, term):
 def derive_url(generated_url):
     driver.get(generated_url)
 
-    #downloadButton = WebDriverWait(driver, 10).until(lambda s: len(s.find_element_by_class_name('download-link button button-green').is_displayed()))
-    driver.implicitly_wait(10)
-    downloadButton = driver.find_element_by_id("download-button-desktop-release")
+    downloadButton = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID. "download-button-desktop-release"))
+    )
+    #driver.implicitly_wait(10)
+    #downloadButton = driver.find_element_by_id("download-button-desktop-release")
     downloadButton.click()
-    driver.implicitly_wait(8)
+    #driver.implicitly_wait(8)
     downloadLink = driver.find_element_by_id("direct-download-link").get_attribute("href")
 
     print "Stub Attribution download link is:\n %s" % downloadLink
